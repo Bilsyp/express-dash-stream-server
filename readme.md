a3a51867075a0cb3f5837e66605737d9d6b57131
bilsyp <bilalsyah77@gmail.com>
2024-01-08 22:27:15 +0700

    complete

---
+++ readme.md
@@ -0,0 +1,48 @@
+# MPEG-DASH Streaming Server
+
+This repository contains a simple Node.js server using Express to handle MPEG-DASH streaming requests. It serves DASH manifest (playlist.mpd) and video segments of different qualities.
+
+### Installation
+
+#### 1. Clone the repository ':'
+
+```bash
+git clone https://github.com/your-username/your-repo.git
+```
+
+#### 2. Install dependencies ':'
+
+```bash
+npm install
+```
+
+#### Usage
+
+```bash
+npm run dev
+```
+
+### Endpoints
+
+### 1. DASH Manifest ':'
+
+- **Endpoint** : /stream/playlist.mpd
+- **Description** : Serves the DASH manifest for initializing the player.
+- **Content-Type** : application/dash+xml
+
+### 2. Video Segments ':'
+
+- **Endpoint** : /stream/:quality/:segment
+- **Description** : Serves video segments based on quality and segment information.
+- **Parameters** :
+
+  - quality - Video quality (e.g., 720p, 1080p).
+  - segment - Segment file name.
+
+- **Content-Type**: video/webm
+
+### Dependencies
+
+- **Express**: Fast, unopinionated, minimalist web framework for Node.js.
+- **cors**: Middleware to enable CORS (Cross-Origin Resource Sharing).
+- **Node.js built-in modules**: fs, http, path.
---
+++ server.js
@@ -0,0 +1,55 @@
+import express from "express";
+import cors from "cors";
+import { readFile } from "node:fs";
+import { promises as fsPromises } from "node:fs";
+import path from "node:path";
+import http from "node:http";
+
+const app = express();
+const server = http.createServer(app);
+app.use(cors());
+
+//  todo playlist.mpd sebagai inisiasi awal pada player
+
+app.get("/stream/playlist.mpd", (req, res) => {
+  const pathname = req.url;
+  const filetype = pathname.substr(1).split(".")[1];
+
+  const dash = readFile(pathname.substr(1), (err, data) => {
+    if (filetype == "mpd") {
+      res.writeHead(200, {
+        "Content-Type": "application/dash+xml",
+        Connection: "keep-alive",
+      });
+      res.write(data);
+    }
+    res.end();
+  });
+});
+
+// todo handle stream request dari berbagai quality dan audio
+
+app.get("/stream/:quality/:segment", async (req, res) => {
+  const { quality, segment } = req.params;
+  const dashPath = `./stream/${quality}/${segment}`;
+
+  try {
+    const data = await fsPromises.readFile(dashPath);
+    const contentType =
+      path.extname(segment) === ".mpd" ? "application/dash+xml" : "video/webm";
+
+    res.writeHead(200, {
+      "Content-Type": contentType,
+      Connection: "keep-alive",
+    });
+
+    res.write(data);
+    res.end();
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+server.listen(3000, () => {
+  console.log("server is running on port 3000");
+});
